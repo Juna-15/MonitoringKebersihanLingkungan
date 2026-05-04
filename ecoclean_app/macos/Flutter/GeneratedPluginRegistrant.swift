5117fe2fc79c50cbf4f3397a7bb067a38c891a5d
Juna-15 <muhammadjndi@gmail.com>
2026-05-04 12:49:20 +0700

    proses2

--- ecoclean_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ecoclean_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,9 @@ import firebase_auth
 import firebase_core
 import geolocator_apple
 import package_info_plus
+import path_provider_foundation
+import share_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
